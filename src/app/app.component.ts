4a0d8b01fbf93d14dbaa5e70cde22451b852af4e
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2024-12-08 07:54:40 +0530

    added my  first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularproject';
+  name="sai";
+  image={
+    src:"https://www.shutterstock.com/shutterstock/photos/2494850607/display_1500/stock-photo-the-iconic-charminar-at-dusk-beautifully-illuminated-against-the-twilight-sky-in-hyderabad-india-2494850607.jpg",alt:"image1"
+  }
 }
